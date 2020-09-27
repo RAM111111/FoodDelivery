706b10836df6e40a2a65c3aa58c25aca25b73cd9
RAM111111 <32421153+RAM111111@users.noreply.github.com>
2020-09-28 00:11:48 +0300

    Initial Commit

---
+++ FoodDeleivery/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoodDeleivery
+//
+//  Created by ر on 10/02/1442 AH.
+//  Copyright © 1442 r. All rights reserved.
+//
+
+import SwiftUI
+class Foodcontroller:UICollectionViewController{
+    
+}
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
